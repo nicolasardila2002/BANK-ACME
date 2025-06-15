89f1e34f582c7c779faf60fd4e81583510a2dfed
Carloscisneroides <cisneroserlianyer@gmail.com>
2025-06-15 07:20:06 -0500

    feat: :art: Avance de recuperera contraseña

---
+++ recuperar-contraseñas/reset.js
@@ -0,0 +1,10 @@
+import { recuperarContrasena } from "../registro/jsprincipal.js";
+
+document.getElementById("reset-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email-reset").value;
+
+    const resultado = await recuperarContrasena(email);
+
+    document.getElementById("mensaje-reset").innerHTML = `<p class='${resultado.success ? "success-message" : "error-message"}'>${resultado.message}</p>`;
+});
